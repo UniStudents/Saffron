f4ecd067ad922b29de79c4db8266838938f48650
Constantinos <rounnus@gmail.com>
2021-04-29 01:01:25 +0300

    The instructions class is complete.

--- src/components/instructions.ts
+++ src/components/instructions.ts
@@ -1,10 +1,61 @@
 import Source from "./source";
 
-export default class Article {
-    declare id: string
-    declare source : {
+/**
+ * The instructions class is used mainly by parsers.
+ * Its general use is to provide instructions on how
+ * parsers will work during the process of analyzing
+ * a web page content.
+ */
+export default class Instructions {
+    private declare source : {
         id: string
     }
-    //TODO add getSource
+    private declare static instructions: Array<Instructions>;
+    declare id: string;
+    declare url: string;
+    declare endPoint: string;
+    declare scrapeOptions: object;
+    declare elementSelector: string;
+    declare sourceType: string;
+
+    /**
+     * @param id instruction id.
+     */
+    constructor(id: string) {
+        this.id = id;
+    }
+
+    /**
+     * We return a Map which contains all the options
+     * that a parser needs.
+     *
+     * @return Map<string, object>
+     */
+    getOptions(): Map<string, object> | null {
+        if (!this.scrapeOptions) return null;
+
+        return new Map<string, object>(Object.entries(this.scrapeOptions));
+    }
+
+    /**
+     * Return all the instructions that exist
+     * in the instructions Array.
+     *
+     * @return Array<Instructions>
+     */
+    static getInstructions(): Array<Instructions> | null {
+        if (!this.instructions) return null;
+
+        return this.instructions;
+    }
+
+    /**
+     * Return the source that variable source refers to.
+     *
+     * @return object
+     */
+    getSource(): object {
+        return this.source;
+    }
 
 }
