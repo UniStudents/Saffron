e543845ba7ed45643ee7128a6c62d0fb4014d5eb
George Andredakis <george@andredakis.gr>
2021-12-14 13:53:29 +0200

    Temp fix for database merge

--- src/modules/database/database.ts
+++ src/modules/database/database.ts
@@ -18,7 +18,8 @@ export default abstract class Database {
     abstract getArticles(src: string, options?: {
         pageNo?: number,
         articlesPerPage?: number,
-        sort?: { [key: string]: -1 | 1 }
+        sort?: { [key: string]: -1 | 1 },
+        filter?: any
     }): Promise<Array<Article>>
 
     /**
@@ -29,7 +30,7 @@ export default abstract class Database {
      * @param article The article object to add to the database
      * @return The article id if it pushed successfully.
      */
-    abstract pushArticle(src: string, article: Article): Promise<string>
+    abstract pushArticle(src: string, article: Article): Promise<boolean>
 
     /**
      * The RECOMMENDED way to push articles to the database.
@@ -38,8 +39,8 @@ export default abstract class Database {
      * @param articles An array of Article objects that are meant to be merged into the database.
      */
     async mergeArticles(src: string, articles: Article[]) {
-        // if(articles.length > 0)
-        Events.emit("workers.articles.found", articles, src)
+        Events.emit("workers.articles.found", articles, src); // Can be empty array
+        if(articles.length == 0) return;
 
         let getExtPair = Extensions.getInstance().startCount();
         let pair: any;
@@ -53,23 +54,39 @@ export default abstract class Database {
             }
         }
 
+        let urls: string[] = articles[0].getSource().instructions.url.map((url: string[]) => url[0]);
+        // if(urls.length > 1) {
+        //     for(const url of urls) {
+        //         let dbArticles = await this.getArticles(src, {
+        //             pageNo: 1,
+        //             articlesPerPage: articles.length >= 5 ? articles.length * 2 : 10,
+        //             filter: {}
+        //         })
+        //
+        //
+        //     }
+        //
+        //     return;
+        // }
+
+
+        let num = urls.length * articles.length // TODO - temporary until search for every url.
         let dbArticles = await this.getArticles(src, {
             pageNo: 1,
-            articlesPerPage: articles.length >= 5 ? articles.length * 2 : 10
+            articlesPerPage: num > 10 ? num : 20
         })
 
         // And then check if they already exist.
-        let hashes = dbArticles.map((article: Article) => article.getHash())
-        articles = articles.filter((article: Article) => !hashes.includes(article.getHash()))
-        if (articles.length > 0)
-            Events.emit("workers.articles.new", articles);
+        let hashes = dbArticles.map((article: Article) => article.getHash());
+        articles = articles.filter((article: Article) => !hashes.includes(article.getHash()));
+        Events.emit("workers.articles.new", articles); // Can be empty array
 
         for (const article of articles) {
-            let id = await this.pushArticle(src, article)
-            if (id == "") Events.emit("workers.articles.errorOffloading", article.toJSON())
+            let added = await this.pushArticle(src, article);
+            if (!added) Events.emit("workers.articles.errorOffloading", article);
         }
 
-        return articles
+        return articles;
     }
 
     abstract insertGridNode(id: string, publicIP: object, privateIP: string, encryptionKey: string): Promise<void>
