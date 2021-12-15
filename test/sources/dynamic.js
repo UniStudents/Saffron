5b346b9c48a318e8d215979f6ecc2fcb7eddeca1
George Andredakis <george@andredakis.gr>
2021-12-15 19:43:08 +0200

    micro changes

--- test/sources/dynamic.js
+++ test/sources/dynamic.js
@@ -1,9 +1,9 @@
-const {types: {Article, Utils, Exceptions}} = require("../../dist/index")
+const {types: {Article, Utils}} = require("../../dist/index")
 const utils = new Utils()
 
 module.exports = {
     url: "https://www.unipi.gr/unipi/el/%CE%B1%CE%BD%CE%B1%CE%BA%CE%BF%CE%B9%CE%BD%CF%8E%CF%83%CE%B5%CE%B9%CF%82.html",
-    name: "custom-cs.unipi.gr",
+    name: "dynamic-cs.unipi.gr",
     type: "dynamic",
     scrape: async () => {
         // const article = new Article()
